3a78bfe2bc0cece1c924bd9426b8d43f1059564c
Peter Jacobo <object.opera@gmail.com>
2018-07-01 01:32:55 -0400

    Initial Setup - Homepage
    Add a blade for the home page. Include styles as a separate css.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,90 +4,25 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+        <link href="{{ asset('css/welcome.css') }}" rel="stylesheet" type="text/css" >
 
-        <title>Laravel</title>
+        <title>Stockbridge</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    @foreach ($links as $link)
-                        <a href="{{ $link->url }}">{{ $link->title }}</a>
-                    @endforeach
-                </div>
+        <form action="/view" method="post">
+            <div class="text-center mb-4">
+                <h1 class="h3 mb-3 font-weight-normal">Stockbridge Search</h1>
+                <p>Search for a magic card from the Core 19 set.</p>
             </div>
-        </div>
+
+            <div class="form-label-group">
+                <input id="cardNameInput" class="form-control" placeholder="Enter card name" type="text">
+                <label for="cardNameInput">Enter card name</label>
+            </div>
+
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+        </form>
     </body>
 </html>
